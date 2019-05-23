a391c947328009262bc0b45f530294aa9cdefeac
Zizhou Zhang <strawhatj@gmail.com>
2019-05-23 11:26:21 +0800

    clip card grid layout functional

--- src/components/GetClips.js
+++ src/components/GetClips.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import ReactDOMServer from 'react-dom/server';
 import ClipComp from './ClipComp';
 import Slider from 'react-slick';
 import 'react-datepicker/dist/react-datepicker.css';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import ReactHtmlParser from 'react-html-parser';
 import './../styles/style.css';
 
 //https://api.twitch.tv/helix/games?name=fortnite gets info on game, name must match
@@ -24,6 +29,7 @@ class GetClips extends Component {
 			loadingClips: false,
 			display: false,
 			url: ``,
+			numClips: 20,
 			data: [],
 			clips: [],
 			options: {
@@ -88,8 +94,46 @@ class GetClips extends Component {
 		this.setState({ category: name, category_id: id });
 	}
 
+	columnizeClips(array) {
+		let total = 1;
+		let all;
+
+		while (total + 3 <= this.state.numClips) {
+			all += `<div class="row">${ReactDOMServer.renderToStaticMarkup(array[total])}
+			${ReactDOMServer.renderToStaticMarkup(array[total + 1])}
+			${ReactDOMServer.renderToStaticMarkup(array[total + 2])}</div>`;
+			total += 3;
+		}
+
+		const diff = this.state.numClips - total;
+
+		switch (diff) {
+			case 0:
+				all += `<div class="row">${ReactDOMServer.renderToStaticMarkup(array[total])}</div>`;
+				break;
+			case 1:
+				all += `<div class="row">${ReactDOMServer.renderToStaticMarkup(array[total])}
+			${ReactDOMServer.renderToStaticMarkup(array[total + 1])}</div>`;
+				break;
+
+			case 2:
+				all += `<div class="row">${ReactDOMServer.renderToStaticMarkup(array[total])}
+			${ReactDOMServer.renderToStaticMarkup(array[total + 1])}
+			${ReactDOMServer.renderToStaticMarkup(array[total + 2])}</div>`;
+				break;
+
+			default:
+		}
+
+		// while (total < array.length) {
+		// 	a += <div>array[total]</div>;
+		// 	total++;
+		// }
+
+		return ReactHtmlParser(all);
+	}
+
 	render() {
-		console.log(this.state.clips);
 		//map fetched games to select list
 		const games = this.state.data.map((game) => {
 			return (
@@ -111,20 +155,24 @@ class GetClips extends Component {
 		//map fetched clips to clip components
 		const clips = this.state.clips.map((clip) => {
 			return (
-				<ClipComp
-					key={clip.id}
-					info={{
-						streamer: clip.broadcaster_name,
-						title: clip.title,
-						date: clip.created_at,
-						thumbnail: clip.thumbnail_url,
-						link: clip.url,
-						language: clip.language,
-						views: clip.view_count.toString()
-					}}
-				/>
+				<Col>
+					<ClipComp
+						key={clip.id}
+						info={{
+							streamer: clip.broadcaster_name,
+							title: clip.title,
+							date: clip.created_at,
+							thumbnail: clip.thumbnail_url,
+							link: clip.url,
+							language: clip.language,
+							views: clip.view_count.toString()
+						}}
+					/>
+				</Col>
 			);
 		});
+
+		console.log(clips);
 
 		const settings = {
 			dots: false
@@ -157,7 +205,11 @@ class GetClips extends Component {
 
 				<div test-data="clipDisplay">
 					{this.state.display &&
-						(this.state.loadingClips ? <img src={'loading.svg'} alt="loading.." /> : <div>{clips}</div>)}
+						(this.state.loadingClips ? (
+							<img src={'loading.svg'} alt="loading.." />
+						) : (
+							<Container>{this.columnizeClips(clips)}</Container>
+						))}
 				</div>
 			</header>
 		);

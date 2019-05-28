47fc1b138ac7f1f8d7a73dc157ef31abdfefd7f7
Zizhou Zhang <strawhatj@gmail.com>
2019-05-28 09:45:44 +0800

    app is now functional comp. undefined bug observed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GetClips from './components/GetClips';
 
-class App extends Component {
-	constructor() {
-		super();
-
-		this.state = {};
-	}
-
-	render() {
-		return <GetClips />;
-	}
-}
+const App = () => {
+	return <GetClips />;
+};
 
 export default App;
